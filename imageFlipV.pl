7d323088d4eacc0347936fe0953dfefb0ecb9330
JorgeMoragaCalvo <jorge.moraga.c@usach.cl>
2022-10-31 11:45:22 -0300

    Implementacion modificador imageFlipV

--- imageFlipV.pl
+++ imageFlipV.pl
@@ -18,3 +18,46 @@
 %PC(1, 0) -> (0, 0) -> X + 1
 %PD(1, 1) -> (0, 1) -> X - 1
 
+
+/*-------------------------------------------------------------------------------------------------------------------------------------*/
+
+pixelFlipV(Width, PixelIn, PixelOut):- (pixbit(X, Y, Bit, Depth, PixelIn) -> ((X < Width - 1 -> NewX is X + 1; NewX is X - 1), 
+    pixbit(NewX, Y, Bit, Depth, PixelOut)); pixrgb(X, Y, R, G, B, Depth, PixelIn) -> (X < Width - 1 -> NewX is X + 1; NewX is X - 1),
+    pixrgb(NewX, Y, R, G, B, Depth, PixelOut)).
+
+pixelsFlipV(_, [], []):- !.
+pixelsFlipV(Width, [PixelIn|PixelsIn], [PixelOut|PixelsOut]):- pixelFlipV(Width, PixelIn, PixelOut),
+    pixelsFlipV(Width, PixelsIn, PixelsOut), !.
+
+imageFlipV(ImageIn, ImageOut):- image(Width, Height, PixelsIn, ImageIn), pixelsFlipV(Width, PixelsIn, PixelsOut),
+    image(Width, Height, PixelsOut, ImageOut).
+
+/*-------------------------------------------------------------------------------------------------------------------------------------*/
+
+
+%pixbit(0,0,1,10, P1), pixbit(0,1,0,20, P2), pixbit(1,0,1,30,P3), pixbit(1,1,1,40,P4), image(2,2, [P1,P2,P3,P4],I), imageFlipV(I, I1); true.
+%P1 = [0, 0, 1, 10],
+%P2 = [0, 1, 0, 20],
+%P3 = [1, 0, 1, 30],
+%P4 = [1, 1, 1, 40],
+%I = [2, 2, [[0, 0, 1, 10], [0, 1, 0, 20], [1, 0, 1, 30], [1, 1, 1, 40]]],
+%I1 = [2, 2, [[1, 0, 1, 10], [1, 1, 0, 20], [0, 0, 1, 30], [0, 1, 1, 40]]].
+
+
+%pixhex(0, 0, "#FFFFFF", 10, P1), pixhex(0, 1, "#FFFFFF", 20, P2), pixhex(1, 0, "#FFFFFF", 30, P3), pixhex(1, 1, "#FFFFFF", 40, P4), image(2, 2, [P1, P2, P3, P4], I), imageFlipV(I, I1); true.
+%P1 = [0, 0, "#FFFFFF", 10],
+%P2 = [0, 1, "#FFFFFF", 20],
+%P3 = [1, 0, "#FFFFFF", 30],
+%P4 = [1, 1, "#FFFFFF", 40],
+%I = [2, 2, [[0, 0, "#FFFFFF", 10], [0, 1, "#FFFFFF", 20], [1, 0, "#FFFFFF", 30], [1, 1, "#FFFFFF", 40]]],
+%I1 = [2, 2, [[1, 0, "#FFFFFF", 10], [1, 1, "#FFFFFF", 20], [0, 0, "#FFFFFF", 30], [0, 1, "#FFFFFF", 40]]].
+
+
+%pixrgb(0,0,0,0,0,10,P1), pixrgb(0,1,10,10,10,20,P2),  pixrgb(1,0,20,20,20,30,P3), pixrgb(1,1,30,30,30,40,P4),image(2,2,[P1,P2,P3,P4], I), 
+%imageFlipV(I, I1); true.
+%P1 = [0, 0, 0, 0, 0, 10],
+%P2 = [0, 1, 10, 10, 10, 20],
+%P3 = [1, 0, 20, 20, 20, 30],
+%P4 = [1, 1, 30, 30, 30, 40],
+%I = [2, 2, [[0, 0, 0, 0, 0, 10], [0, 1, 10, 10, 10, 20], [1, 0, 20, 20, 20, 30], [1, 1, 30, 30, 30, 40]]],
+%I1 = [2, 2, [[1, 0, 0, 0, 0, 10], [1, 1, 10, 10, 10, 20], [0, 0, 20, 20, 20, 30], [0, 1, 30, 30, 30, 40]]].
